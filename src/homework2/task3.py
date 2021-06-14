5fab4569879c8c3430db6f5c77fe1adb28ab1a5c
Jurij Stankewicz <jurasach@mail.ru>
2021-06-14 11:13:24 +0300

    homework2 task3 correct

--- src/homework2/task3.py
+++ src/homework2/task3.py
@@ -1,8 +1,11 @@
 # Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
-new_string=str(input('Enter please new string: ')) # Вводим строку для задачи
-without_tab_new_string = new_string.replace(" ","")# Удаляем все пробелы
+new_string = str(input('Enter please new string: '))  # Вводим строку для задачи
+without_tab_new_string = new_string.replace(" ", "")  # Удаляем все пробелы
 
-result="".join(set(without_tab_new_string)) # Удаляем все повторяющиеся символы
+result=str("".join(set(without_tab_new_string)))  # Удаляем все повторяющиеся символы
+b = sorted(result) # Переводим строку в список и сортируем по алфавиту согласно условия
+c = ''.join(b) # Переводим список обратно в строку символов
+print(c) # Выводим результат
 
-print (result) # Выводим результат
+
 
