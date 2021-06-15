c7a4b068d51521fd1295f4d8c9eec01ee47b1080
Jurij Stankewicz <jurasach@mail.ru>
2021-06-15 14:16:23 +0300

    task8_1 correct2

--- src/homework2/task8_1.py
+++ src/homework2/task8_1.py
@@ -1,7 +1,8 @@
-#  Your task is to make a function that can take any non-negative integer as an argument and return it with its
-#    digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+# Ваша задача состоит в том, чтобы создать функцию, которая может принимать любое неотрицательное целое число в
+# качестве аргумента и возвращать его   цифры в порядке убывания. По сути, переставьте цифры, чтобы создать
+# максимально возможное число.
 
-num = int(input("Enter please number: "))# Ввели число перевели его в список и список отсортировали
+num = int(input("Enter please number: "))  # Ввели число перевели его в список и список отсортировали
 result_1 = [int(x) for x in str(num)]
 result_1.sort(reverse=True)
 result = int(''.join(map(str, result_1)))
