d9fa9fd7f217e17a942fbb1e0d25147261cc6b37
Jurij Stankewicz <jurasach@mail.ru>
2021-06-16 12:52:59 +0300

    2

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,3 +1,4 @@
+
 # Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания
 import re
 
@@ -5,6 +6,5 @@ new_string = (input('Enter please the sentence: '))  # Вводим предло
 correct_string = re.sub(r'[^\w\s]', ' ',
                         new_string)  # Заменим все знаки препинания на пробели запишем все это в новую строку
 #  Будем использовать re.sub(pattern, replacement, original_string) - библиотеку регулярных выражений
-
 count = max(correct_string.split(), key=len)  # Находим самое длинное слово
 print(count)  # Выводим результат
