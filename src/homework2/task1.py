2c8063b6a5e51a2cfd3eabf663b7791c4716a247
Jurij Stankewicz <jurasach@mail.ru>
2021-06-18 10:31:49 +0300

    работа над ошибками

--- src/homework2/task1.py
+++ src/homework2/task1.py
@@ -7,5 +7,6 @@ rubles = int(input('Enter please price in rubles: '))
 kopecks = int(input('Enter please price in kopecks: '))
 price_in_kopeck = rubles * 100 + kopecks  # Переводим рубли в копейки
 price_finish = price_in_kopeck * count  # Считаем общую цену товара
-print("\rTotal amound: " + str(price_finish // 100) +
-      " rubles " + str(price_finish % 100) + " kopeck. ")
+print("\rTotal amount: {0} rubles {1} kopeck. ".
+      format(str(price_finish // 100),
+             str(price_finish % 100)))
