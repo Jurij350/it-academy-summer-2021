652705b55b098f1c348b460d09dd211f00fddc2a
Jurij Stankewicz <jurasach@mail.ru>
2021-06-21 11:39:22 +0300

    homework2 task4 corr

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -1,6 +1,5 @@
 # Посчитать количество строчных и прописных букв
 # в веденной строке. Учитывать только английские буквы.
-from string import ascii_letters as en
 
 new_str: str = str(input("Enter please string: "))
 # Вводим строку для контроля
@@ -8,9 +7,10 @@ index_low = 0  # индекс для подсчета строчных букв
 index_up = 0  # Индекс для подсчета прописных букв
 result = list(new_str)
 for i in result:  # Считаем английские буквы в нашем массиве через цикл
-    if i.islower() and i in en:
+    if 'a' <= i <= 'z':
         index_low = index_low + 1
-    if i.isupper() and i in en:
+    if 'A' <= i <= 'Z':
         index_up = index_up + 1
+
 print("Lowercase: " + str(index_low))
 print("Uppercase: " + str(index_up))
