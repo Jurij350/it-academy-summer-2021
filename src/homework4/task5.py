1fd67142be7c64199751e07ff681f8e8a36e9a95
Jurij Stankewicz <jurasach@mail.ru>
2021-06-24 18:17:46 +0300

    Homework4 task5

---
+++ src/homework4/task5.py
@@ -0,0 +1,33 @@
+# Каждый из N школьников некоторой школы знает
+# Mi языков. Определите, какие языки знают
+# все школьники и языки, которые знает
+# хотя бы один из школьников.
+
+# Входные данные
+schoolboys = 3
+schoolboys_1 = [1, 'Russian', 'English']
+schoolboys_2 = [2, 'Russian', 'Belarusian', 'English']
+schoolboys_3 = [3, 'Russian', 'Italian', 'French']
+
+# Выходные данные
+print(schoolboys)
+# Выводим количество школьников
+result_languages = set(schoolboys_1[1::]
+                       + schoolboys_2[1::]
+                       + schoolboys_3[1::])
+
+print(result_languages)
+# Выводим список всех языков. Для этого
+# используем множество, по скольку
+# данный тип данных выводит только
+# уникальные значения.
+result_languages_knouledge = set(schoolboys_1[1::]) \
+                             & set(schoolboys_2[1::]) \
+                             & set(schoolboys_3[1::])
+# Разность трех множеств - это есть языки,
+# которые знает хотя бы один школьник
+# Выведем для начала количество этих языков
+print(len(result_languages_knouledge))
+# А затем выведем языки которые
+# знает хотя бы один школьник
+print(result_languages_knouledge)
