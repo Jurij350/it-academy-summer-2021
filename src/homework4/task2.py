07e7c8fb8a989a694f5fef04210825fdb35adc47
Jurij Stankewicz <jurasach@mail.ru>
2021-06-25 09:17:06 +0300

    Homework4-2 слишком длинные строки

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -14,12 +14,14 @@ input_list2 = input_string2.split(' ')
 input_list3 = input_string3.split(' ')
 # Создаем словарь где ключем является название
 # страны а значием является список городов
-new_dict = {input_list1[0]: (input_list1[1::]), input_list2[0]: (input_list2[1::])}
+new_dict = {input_list1[0]: (input_list1[1::]),
+            input_list2[0]: (input_list2[1::])}
 # В данном вдойном цикле мы сравниваем элементы
 # третьего цикла с городами для проверки
 # и города в нашем словаре
 # и при совпадении значения выводим значение ключа
 for i in range(len(input_list3)):
     for k, v in new_dict.items():
-        if (isinstance(v, list) and input_list3[i] in v) or input_list3[i] == v:
+        if (isinstance(v, list) and input_list3[i] in v) \
+                or input_list3[i] == v:
             print(k)
