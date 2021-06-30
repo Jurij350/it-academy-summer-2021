ec7bbbd9331a8382e08c039239ffbb1eb8281a46
Jurij Stankewicz <jurasach@mail.ru>
2021-06-30 17:29:16 +0300

    Homework4 - task5-corrected

--- src/homework4/task5.py
+++ src/homework4/task5.py
@@ -9,22 +9,22 @@ schoolboys_1 = [1, 'Russian', 'English']
 schoolboys_2 = [2, 'Russian', 'Belarusian', 'English']
 schoolboys_3 = [3, 'Russian', 'Italian', 'French']
 
-# Выходные данные
-print(schoolboys)
 # Выводим количество школьников
-result_languages = set(
-    schoolboys_1[1::] + schoolboys_2[1::] + schoolboys_3[1::])
-print(result_languages)
+print(schoolboys)
 # Выводим список всех языков. Для этого
 # используем множество, по скольку
 # данный тип данных выводит только
 # уникальные значения.
+result_languages = set(
+    schoolboys_1[1::] + schoolboys_2[1::] + schoolboys_3[1::])
+print(result_languages)
+# Разность трех множеств - это есть языки,
+# которые знает хотя бы один школьник
 result_languages_knowledge = set(
     schoolboys_1[1::]) & set(
     schoolboys_2[1::]) & set(
     schoolboys_3[1::])
-# Разность трех множеств - это есть языки,
-# которые знает хотя бы один школьник
+
 # Выведем для начала количество этих языков
 print(len(result_languages_knowledge))
 # А затем выведем языки которые
