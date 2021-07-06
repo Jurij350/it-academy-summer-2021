f6f60d3602c3ee2105157853a1a466d9e33e6285
Jurij Stankewicz <jurasach@mail.ru>
2021-07-06 12:39:55 +0300

    Homework4 - task2 -

---
+++ src/homework5/task2.py
@@ -0,0 +1,36 @@
+# Создайте декоратор, который хранит результаты
+# вызовов функции (за все время вызовов, не только
+# текущий запуск программы)
+
+# Это переменная для звписи вызовов функции
+result_list = []
+# Сам декоратор который хранит записывает
+# данные в наш список
+
+def my_first_dec(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result_list.append(result)
+        print(result_list)
+        return result
+
+    return wrapper
+
+# Обертка двух функций в декоратор
+@my_first_dec
+def func_multiply(a):
+    return a * a
+
+
+@my_first_dec
+def func_sum(b):
+    return b + b
+
+# Вызов функций. По результату мы видим что в
+# список добавляется новое значение при следующем
+# вызове функции. Соответственно старое значение
+# тоже сохраняется
+result_func_1 = func_multiply(4)
+result_func_2 = func_sum(5)
+
+
